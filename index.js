f856959ad7d1019d14b9da7bf3bf1250c4204726
Samir Shaikh <sameershaikh84663@gmail.com>
2025-01-01 16:38:21 +0530

    some changes

--- index.js
+++ index.js
@@ -10,9 +10,9 @@ import connectToDatabase from "./db/connect.js";
 dotenv.config();
 
 const app = express();
-// (async () => {
-//   await connectToDatabase(); // Connect to MongoDB
-// })();
+(async () => {
+  await connectToDatabase(); // Connect to MongoDB
+})();
 
 // Middleware
 app.use(cors());
@@ -23,20 +23,20 @@ app.use(bodyParser.json()); // body-parser usage with ES module
 
 // Connect to MongoDB
 // At the MongoDB connection
-mongoose.connect(process.env.MONGO_URI,{serverSelectionTimeoutMS: 130000 })
-  .then(() => {
-    console.log("MongoDB Connected");
-    console.log("Current database:", mongoose.connection.db.databaseName);
-    // List all collections
-    mongoose.connection.db.listCollections().toArray((err, collections) => {
-      if (err) {
-        console.log("Error getting collections:", err);
-      } else {
-        console.log("Available collections:", collections.map(c => c.name));
-      }
-    });
-  })
-  .catch((err) => console.error(err));
+// mongoose.connect(process.env.MONGO_URI,{serverSelectionTimeoutMS: 130000 })
+//   .then(() => {
+//     console.log("MongoDB Connected");
+//     console.log("Current database:", mongoose.connection.db.databaseName);
+//     // List all collections
+//     mongoose.connection.db.listCollections().toArray((err, collections) => {
+//       if (err) {
+//         console.log("Error getting collections:", err);
+//       } else {
+//         console.log("Available collections:", collections.map(c => c.name));
+//       }
+//     });
+//   })
+//   .catch((err) => console.error(err));
 
   // main page 
   app.get("/", (req, res) => {
