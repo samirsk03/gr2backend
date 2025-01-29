90f22e936cbe6e2142578cb568ae58e12c73f388
Samir ibn Shoukat <145380646+samirsk03@users.noreply.github.com>
2025-01-29 14:12:23 +0530

    Delete models directory

--- models/Form.js
+++
@@ -1,9 +0,0 @@
-import { Schema, model } from "mongoose";
-
-const FormSchema = new Schema({
-  name: String,
-  email: String,
-  message: String,
-});
-
-export default model("Form", FormSchema, "Form");
