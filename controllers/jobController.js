96099f707d2cda907efdd58cc348c6fc0cc04bc1
Samir Shaikh <sameershaikh84663@gmail.com>
2025-01-30 20:20:58 +0530

    applicant routes created

--- controllers/jobController.js
+++ controllers/jobController.js
@@ -1,4 +1,5 @@
 const Job = require('../models/jobModel');
+const Applicant = require('../models/applicantsModel')
 
 // Create a new job posting
 exports.createJob = async (req, res) => {
@@ -27,8 +28,8 @@ exports.applyForJob = async (req, res) => {
     // Automatically set roleAppliedFor as job title
     const roleAppliedFor = job.title;
 
-    job.applicants.push({ name, email, linkedInProfile, coverLetter, roleAppliedFor, resume });
-    await job.save();
+    const applicants = Applicant({ name, email, linkedInProfile, coverLetter, roleAppliedFor: job.title , resume });
+    await applicants.save();
 
     res.status(200).json({ message: 'Application submitted successfully!' });
   } catch (error) {
@@ -36,8 +37,22 @@ exports.applyForJob = async (req, res) => {
   }
 };
 
+// delete single job 
+exports.deleteJob = async (req, res) => {
+  try {
+    const {jobId} = req.params ;
 
-// Fetch all contacts
+    const job = await Job.findByIdAndDelete(jobId);
+    if(!job) {
+      return res.status(404).json({message: "job did not found"});
+    } else res.status(200).json({message: 'job deleted successfully'})
+  } catch (error) {
+    res.status(500).json({error : error.message});
+  }
+}
+
+
+// Fetch all jobs
 exports.getAllJobs = async (req, res) => {
   try {
     const job = await Job.find().sort({ createdAt: -1 }); // Fetch job sorted by newest first
@@ -46,3 +61,28 @@ exports.getAllJobs = async (req, res) => {
     res.status(500).json({ error: error.message });
   }
 };
+
+// fetch all applicants
+exports.getAllApplicants = async (req, res) => {
+  try {
+    const allApplicants = await Applicant.find().sort({appliedAt: -1})
+    res.status(200).json(allApplicants);
+  } catch (error){
+    res.status(500).json({ error: error.message });
+  }
+}
+
+// delete applicant by id 
+exports.deleteApplicant = async (req, res) => {
+  try {
+    const {applicantId} = req.params;
+
+    const applicant = await Applicant.findByIdAndDelete(applicantId);
+    if(!applicant) {
+      return res.status(404).json({message: "applicant did not found"});
+    } else res.status(200).json({message: 'applicant deleted successfully'})
+  } catch (error) {
+    res.status(500).json({error : error.message});
+  
+  }
+}
